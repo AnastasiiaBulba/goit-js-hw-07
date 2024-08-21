ec2b5c201d9099d6aad78ea0b33816f08b5525cd
Anastasiia Bulba <fedinanastya@gmail.com>
2024-08-21 11:09:56 +0300

    fear sixs task(((

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const formSubmit = document.querySelector('.login-form');
+formSubmit.addEventListener('submit', submitForm);
 
+function submitForm(event) {
+  event.preventDefault();
+  const globalForm = event.currentTarget;
+  const emailForm = globalForm.elements.email.value.trim();
+  const passwordForm = globalForm.elements.password.value.trim();
+
+  if (emailForm === '' || passwordForm === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const collectForm = {
+    email: emailForm,
+    password: passwordForm,
+  };
+  console.log(collectForm);
+  globalForm.reset();
+}
